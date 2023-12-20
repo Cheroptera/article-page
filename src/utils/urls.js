ab8568863aaf6c6b2efaceec13e403e3006b0205
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-12-20 11:04:50 +0100

    fixed deployment bug

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://json-server-news-site.onrender.com/'
+const BASE_URL = 'https://json-server-news-site.onrender.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
